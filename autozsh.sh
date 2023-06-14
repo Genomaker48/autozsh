9ee4de317e73bdc6add52776fac11168303d5ed0
genomaker48 <109393193+Genomaker48@users.noreply.github.com>
2023-06-13 22:15:37 -0400

    Update autozsh.sh

--- autozsh.sh
+++ autozsh.sh
@@ -5,26 +5,20 @@ sleep 10
 echo "Empezando."
 sleep 5
 sudo pacman -S zsh
-mkdir ~/Escritorio/repos/
-cd ~/Escritorio/repos
-git clone https://github.com/genomaker48/autozsh
-cd autozsh
 mv .zshrc ~/
 sudo mkdir /usr/share/zsh/plugins/
 sudo mkdir /usr/share/zsh/plugins/zsh-sudo-plugin/
 sudo mkdir /usr/share/zsh/plugins/zsh-autocomplete/
 sudo mkdir /usr/share/zsh/plugins/zsh-autosuggestions/
 sudo mkdir /usr/share/zsh/plugins/zsh-syntax-highlighting/
-sudo mkdir /usr/share/zsh/plugins/zsh-theme-powerlevel10k/
 sudo mv plugins/sudo.plugin.zsh
 sudo mv plugins/zsh-autocomplete.plugin.zsh
 sudo mv plugins/zsh-autosuggestions.zsh
 sudo mv plugins/zsh-syntax-highlighting.zsh
-sudo mv plugins/zsh-theme-powerlevel10k/
-echo "Introduzca su nombre de usuario por favor"
-cat  >> user.sh
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
+echo 'source ~/powerlevel10k/powerlevel10k.zsh-theme' >>~/.zshrc
 echo "Terminando."
 sleep 5
-sudo usermod -s /bin/zsh ./user.sh
+sudo usermod -s /bin/zsh $USER
 echo "Ya esta todo listo"
 sleep 5
