bd7da04f08263e3aac23a09f7c4c85206d89022e
genomaker48 <109393193+Genomaker48@users.noreply.github.com>
2023-07-12 22:34:42 -0400

    Update autozsh.sh

--- autozsh.sh
+++ autozsh.sh
@@ -1,24 +1,41 @@
 #!/bin/sh
 
-echo "Bienvenido, este programa instalara en tu equipo zsh, ademas de que zsh sera tu shell principal."
-sleep 10 
-echo "Empezando."
-sleep 5
-sudo pacman -S zsh
-mv .zshrc ~/
-sudo mkdir /usr/share/zsh/plugins/
-sudo mkdir /usr/share/zsh/plugins/zsh-sudo-plugin/
-sudo mkdir /usr/share/zsh/plugins/zsh-autocomplete/
-sudo mkdir /usr/share/zsh/plugins/zsh-autosuggestions/
-sudo mkdir /usr/share/zsh/plugins/zsh-syntax-highlighting/
-sudo mv plugins/sudo.plugin.zsh
-sudo mv plugins/zsh-autocomplete.plugin.zsh
-sudo mv plugins/zsh-autosuggestions.zsh
-sudo mv plugins/zsh-syntax-highlighting.zsh
-git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
-echo 'source ~/powerlevel10k/powerlevel10k.zsh-theme' >>~/.zshrc
-echo "Terminando."
-sleep 5
-sudo usermod -s /bin/zsh $USER
-echo "Ya esta todo listo"
-sleep 5
+green="\e[0;32m\033[1m"
+blue="\e[0;34m\033[1m"
+
+function start(){
+    echo -e "${green}(BetaExclusivoParaArchLinux)        Bienvenido, este programa instalara en tu equipo zsh, además de volverse tu shell principal.        (BetaExclusivoParaArchLinux)"
+    sleep 10 
+    echo -e "${green}Empezando."
+    sleep 5
+    sudo pacman -S zsh
+    mv .zshrc ~/
+    sudo mkdir /usr/share/zsh/plugins/
+    sudo mkdir /usr/share/zsh/plugins/zsh-sudo-plugin/
+    sudo mkdir /usr/share/zsh/plugins/zsh-autocomplete/
+    sudo mkdir /usr/share/zsh/plugins/zsh-autosuggestions/
+    sudo mkdir /usr/share/zsh/plugins/zsh-syntax-highlighting/
+    sudo mv plugins/sudo.plugin.zsh
+    sudo mv plugins/zsh-autocomplete.plugin.zsh
+    sudo mv plugins/zsh-autosuggestions.zsh
+    sudo mv plugins/zsh-syntax-highlighting.zsh
+    git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
+    echo 'source ~/powerlevel10k/powerlevel10k.zsh-theme' >>~/.zshrc
+    echo -e "${green}Terminando."
+    sleep 5
+    sudo usermod -s /bin/zsh $USER
+    sudo chsh -s /bin/zsh $USER
+    sudo su
+    usermod -s /bin/zsh root
+    chsh -s /bin/zsh root
+    exit
+    echo -e "${green}Ya esta todo listo"
+    sleep 5
+}
+if [ "$(id -u)" == "0" ]; then
+	echo -e "\n${blue}[!] No hay necesidad de ser root para usar esta utilidad${end}"
+	echo
+	exit 1
+else
+	start
+fi
