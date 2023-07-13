0c971c1df94bfa76d1169443d5328332a82e98f6
genomaker48 <109393193+Genomaker48@users.noreply.github.com>
2023-07-12 22:55:45 -0400

    Update autozsh.sh

--- autozsh.sh
+++ autozsh.sh
@@ -8,17 +8,17 @@ function start(){
     sleep 10 
     echo -e "${green}Empezando."
     sleep 5
-    sudo pacman -S zsh
+    sudo pacman -S zsh --needed --noconfirm
     mv .zshrc ~/
     sudo mkdir /usr/share/zsh/plugins/
     sudo mkdir /usr/share/zsh/plugins/zsh-sudo-plugin/
     sudo mkdir /usr/share/zsh/plugins/zsh-autocomplete/
     sudo mkdir /usr/share/zsh/plugins/zsh-autosuggestions/
     sudo mkdir /usr/share/zsh/plugins/zsh-syntax-highlighting/
-    sudo mv plugins/sudo.plugin.zsh
-    sudo mv plugins/zsh-autocomplete.plugin.zsh
-    sudo mv plugins/zsh-autosuggestions.zsh
-    sudo mv plugins/zsh-syntax-highlighting.zsh
+    sudo mv plugins/sudo.plugin.zsh /usr/share/zsh/plugins/zsh-sudo-plugin
+    sudo mv plugins/zsh-autocomplete.plugin.zsh /usr/share/zsh/plugins/zsh-autocomplete
+    sudo mv plugins/zsh-autosuggestions.zsh /usr/share/zsh/plugins/zsh-autosuggestions
+    sudo mv plugins/zsh-syntax-highlighting.zsh /usr/share/zsh/plugins/zsh-syntax-highlighting
     git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
     echo 'source ~/powerlevel10k/powerlevel10k.zsh-theme' >>~/.zshrc
     echo -e "${green}Terminando."
