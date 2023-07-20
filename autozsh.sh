055e46d92f6891a055089baba49a030c88f1c85a
genomaker48 <109393193+Genomaker48@users.noreply.github.com>
2023-07-20 17:52:06 -0400

    Update autozsh.sh

--- autozsh.sh
+++ autozsh.sh
@@ -4,31 +4,29 @@ green="\e[0;32m\033[1m"
 blue="\e[0;34m\033[1m"
 
 function start(){
-    echo -e "${green}(BetaExclusivoParaArchLinux)        Bienvenido, este programa instalara en tu equipo zsh, además de volverse tu shell principal.        (BetaExclusivoParaArchLinux)"
+    echo -e "${green}(SoloArch) Bienvenido, este programa instalara en tu equipo zsh, además de volverse tu shell principal. (SoloArch)"
     sleep 10 
     echo -e "${green}Empezando."
     sleep 5
-    sudo pacman -S zsh --needed --noconfirm
+    sudo pacman -S zsh lsd bat nerd-fonts awesome-nerd-fonts --needed --noconfirm
     mv .zshrc ~/
     sudo mkdir /usr/share/zsh/plugins/
     sudo mkdir /usr/share/zsh/plugins/zsh-sudo-plugin/
     sudo mkdir /usr/share/zsh/plugins/zsh-autocomplete/
     sudo mkdir /usr/share/zsh/plugins/zsh-autosuggestions/
     sudo mkdir /usr/share/zsh/plugins/zsh-syntax-highlighting/
-    sudo mv plugins/sudo.plugin.zsh /usr/share/zsh/plugins/zsh-sudo-plugin
-    sudo mv plugins/zsh-autocomplete.plugin.zsh /usr/share/zsh/plugins/zsh-autocomplete
-    sudo mv plugins/zsh-autosuggestions.zsh /usr/share/zsh/plugins/zsh-autosuggestions
-    sudo mv plugins/zsh-syntax-highlighting.zsh /usr/share/zsh/plugins/zsh-syntax-highlighting
+    sudo mv plugin/sudo.plugin.zsh /usr/share/zsh/plugins/zsh-sudo-plugin
+    sudo mv plugin/zsh-autocomplete.plugin.zsh /usr/share/zsh/plugins/zsh-autocomplete
+    sudo mv plugin/zsh-autosuggestions.zsh /usr/share/zsh/plugins/zsh-autosuggestions
+    sudo mv plugin/zsh-syntax-highlighting.zsh /usr/share/zsh/plugins/zsh-syntax-highlighting
     git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
     echo 'source ~/powerlevel10k/powerlevel10k.zsh-theme' >>~/.zshrc
     echo -e "${green}Terminando."
     sleep 5
     sudo usermod -s /bin/zsh $USER
     sudo chsh -s /bin/zsh $USER
-    sudo su
-    usermod -s /bin/zsh root
-    chsh -s /bin/zsh root
-    exit
+    sudo usermod -s /bin/zsh root
+    sudo chsh -s /bin/zsh root
     echo -e "${green}Ya esta todo listo"
     sleep 5
 }
